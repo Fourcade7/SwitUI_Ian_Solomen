dd9dbacd94315de0c443ee15e94d7f678c1b456c
fourcade7 <FourcadeAminov7@gmail.com>
2024-03-03 16:37:50 +0500

    workwitjtext

--- SwitUI_Ian_Solomen/2_Text/WorkWIthText.swift
+++ SwitUI_Ian_Solomen/2_Text/WorkWIthText.swift
@@ -15,7 +15,7 @@ private struct lesson2_0:View{
         VStack{
             
             
-            Text("Any SwiftUI view can have its corners rounded using the cornerRadius() modifier. This takes a simple value in points that controls how pronounced the rounding should be.")
+            Text("Any SfwiftUI view can have its corners rounded using the cornerRadius() modifier. This takes a simple value in points that controls how pronounced the rounding should be.")
                 .padding()
                 .lineLimit(nil)
                 .font(.title2)
